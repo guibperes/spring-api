5ab9ab72b6f6c2c717ab87db2708bd4525308567
Guilherme Beidaki Peres <guibperes@gmail.com>
2019-02-26 15:15:34 -0300

    user get by token added

--- src/main/java/guizao/aula/api/user/ApiUserController.java
+++ src/main/java/guizao/aula/api/user/ApiUserController.java
@@ -7,11 +7,13 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import guizao.aula.auth.role.annotation.RoleApiUser;
 import guizao.aula.config.RestConfig;
 import guizao.aula.exception.BadRequestException;
 import guizao.aula.exception.EntityNotFoundException;
@@ -19,6 +21,7 @@ import guizao.aula.utils.Login;
 import guizao.aula.utils.Token;
 import io.swagger.annotations.Api;
 import springfox.documentation.annotations.ApiIgnore;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestConfig
 @Api(tags = "User")
@@ -47,5 +50,13 @@ public class ApiUserController {
       throw new EntityNotFoundException("User", login.getUsername() + ":" + login.getPassword());
     }
     throw new BadRequestException("User", err);
+  }
+
+  @RoleApiUser
+  @GetMapping
+  public ResponseEntity<ApiUser> get () {
+    String token = ((ApiUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getToken();
+    Optional<ApiUser> user = userService.findByToken(token);
+    return new ResponseEntity<ApiUser>(user.get(), HttpStatus.OK);
   }
 }
